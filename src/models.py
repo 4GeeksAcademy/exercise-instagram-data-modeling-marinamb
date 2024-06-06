cc8245f31d1e8faba508fac2e411545e55f915b5
Marina <150665821+Marinamb19@users.noreply.github.com>
2024-06-06 18:39:22 +0000

    modelo instagram marinamb

--- src/models.py
+++ src/models.py
@@ -1,32 +1,64 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User_private(Base):
+    __tablename__='User Private'
+    user_private_id= Column(Integer, primary_key=True)
+    user_name=Column(String(15), nullable=False, unique= True)
+    password=Column(String(15), nullable=False)
+    email= Column(String(20), nullable=False, unique=True)
 
-    def to_dict(self):
-        return {}
+
+class User_public (Base):
+    __tablename__='User Public'
+    user_public_id= Column(Integer, primary_key=True)
+    user_name=Column(String(15), nullable=False, unique= True)
+    description = Column(String(250), nullable=True, unique=True)
+    user_private_id = Column(Integer, ForeignKey(User_private.user_private_id))
+
+
+
+class Images (Base):
+    __tablename__='Images'
+    images_id = Column(Integer, primary_key=True)
+    profile_photo = Column(Boolean) 
+    user_public_id=Column(Integer, ForeignKey(User_public.user_public_id))
+
+class Post (Base):
+    __tablename__='Post'
+    post_id= Column(Integer, primary_key=True)
+    user_public_id=Column(Integer, ForeignKey(User_public.user_public_id))
+    image_id = Column(Integer, ForeignKey(Images.images_id)) 
+    post = Column(Enum, nullable=False)
+
+
+class Likes(Base):
+    __tablename__='Likes'
+    like_id=Column(Integer, primary_key=True)
+    likes= Column(Integer, nullable=True)
+    post_id = Column(Integer, ForeignKey(Post.post_id))
+    user_liking_id = Column (Integer, ForeignKey(User_public.user_public_id))
+
+class Feed (Base):
+    __tablename__='Feed'  
+    feed_id=Column(Integer, primary_key=True)
+    user_public_id= Column(Integer, ForeignKey(User_public.user_public_id))
+    post= Column(String, unique=True)
+    post_id =Column(Integer, ForeignKey(Post.post_id))
+
+class Followers (Base):
+    __tablename__='Followers'
+    follower_id= Column(Integer, primary_key=True)
+    user_followers=Column(Integer, ForeignKey(User_public.user_public_id))
+    user_following=Column(Integer, ForeignKey(User_public.user_public_id))
+
 
 ## Draw from SQLAlchemy base
 try:
